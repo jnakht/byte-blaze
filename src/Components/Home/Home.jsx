e68a89df44c31c6ffc4e3bbb92ca1513ad980ed4
jnakht <jisan.cse.cu@gmail.com>
2025-03-09 23:20:35 +0600

    byteblaze text animation added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <div className="hero bg-base-200 min-h-[calc(100vh-120px)]  relative">
                 <div className="hero-content text-center -mt-18">
                     <div className="max-w-lg">
-                        <h1 className="text-5xl font-bold">Welcome to <span className="text-secondary">ByteBlaze</span></h1>
+                        <h1 className="text-5xl font-bold">Welcome to <span className="text-4xl font-extrabold bg-gradient-to-r from-primary via-blue-500 to-secondary text-transparent bg-clip-text bg-300% animate-gradient">ByteBlaze</span></h1>
                         <p className="py-6">
                             ByteBlaze is the bridge between the complex world of technology and the curious minds eager to understand it
                         </p>
