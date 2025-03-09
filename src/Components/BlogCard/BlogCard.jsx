27d0081f178ca64840a4752c9adf2f11d115c821
jnakht <jisan.cse.cu@gmail.com>
2025-03-10 01:46:20 +0600

    alternative image added on blog Card

--- src/Components/BlogCard/BlogCard.jsx
+++ src/Components/BlogCard/BlogCard.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-
+import altImage from '../../assets/404.jpg'
 
 const BlogCard = ({blog}) => {
-    const {cover_image, title} = blog;
+    const {cover_image, title, id, published_at, description} = blog;
     return (
-        <Link to='/' className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-gray-900 dark:bg-gray-50">
-        <img role="presentation" className="object-cover w-full rounded h-44 bg-gray-500 dark:bg-gray-500" src={cover_image} />
+        <Link to={`/blogs/${id}`} className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-gray-900 dark:bg-gray-50">
+        <img role="presentation" className="object-cover w-full rounded h-44 bg-gray-500 dark:bg-gray-500" src={cover_image || altImage} />
         <div className="p-6 space-y-2">
             <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
             <span className="text-xs text-gray-400 dark:text-gray-600">January 21, 2021</span>
