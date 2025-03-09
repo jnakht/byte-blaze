fd9b555ac8b05a4082be777efbcfbb5593c4af5d
jnakht <jisan.cse.cu@gmail.com>
2025-03-09 21:28:43 +0600

    home done without the svg

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,9 +2,11 @@
 
 const Footer = () => {
     return (
-        <div>
-            <h3>This is footer</h3>
-        </div>
+        <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+            <aside>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by ByteBlaze</p>
+            </aside>
+        </footer>
     );
 };
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 
@@ -7,7 +7,29 @@ const Home = () => {
     return (
         <div>
 
-            <h3>This is home sectoin</h3>
+            <div className="hero bg-base-200 min-h-[calc(100vh-120px)] border-2 border-red-500">
+                <div className="hero-content text-center">
+                    <div className="max-w-lg">
+                        <h1 className="text-5xl font-bold">Welcome to <span className="text-secondary">ByteBlaze</span></h1>
+                        <p className="py-6">
+                            ByteBlaze is the bridge between the complex world of technology and the curious minds eager to understand it
+                        </p>
+                        <div className="flex gap-3 justify-center items-center" >
+                            <Link to='/blogs' class="relative inline-block px-4 py-2 font-medium group">
+                                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-primary"></span>
+                                <span class="relative text-black group-hover:text-white">Read Blogs</span>
+                            </Link>
+                            <Link to='/bookmarks' class="relative inline-block px-4 py-2 font-medium group">
+                                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-primary"></span>
+                                <span class="relative text-black group-hover:text-white">Bookmarks</span>
+                            </Link>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
