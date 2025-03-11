251baf2f55c65cc381778105fd0ca681cdca17ba
jnakht <jisan.cse.cu@gmail.com>
2025-03-11 20:59:12 +0600

    first demo big blog link works

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -5,11 +5,12 @@ import BlogCard from "../BlogCard/BlogCard";
 const Blogs = () => {
     const blogs = useLoaderData();
     console.log(blogs)
+    console.log("id go id: ", blogs[0].id)
     return (
         <div>
             <section className="bg-gray-800 dark:bg-gray-100 text-gray-100 dark:text-gray-800">
                 <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-                    <Link to='/' className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900 dark:bg-gray-50">
+                    <Link to={`/blogs/${blogs[0].id}`} className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900 dark:bg-gray-50">
                         <img src={blogs[0].social_image} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-500 dark:bg-gray-500" />
                         <div className="p-6 space-y-2 lg:col-span-5">
                             <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">{blogs[0].title}</h3>
--- src/Components/DetailedBlog/DetailedBlog.jsx
+++ src/Components/DetailedBlog/DetailedBlog.jsx
@@ -10,7 +10,7 @@ const DetailedBlog = () => {
     const [activeCard, setActiveCard] = useState(0);
     return (
         <div>
-            <div className="max-w-2xl px-6 py-16 mx-auto space-y-12">
+            <div className="max-w-[70vw] px-6 py-16 mx-auto space-y-12">
                 <article className="space-y-8 dark:bg-gray-100 dark:text-gray-900">
                     <div className="space-y-6">
                         <h1 className="text-4xl font-bold md:tracking-tight md:text-5xl">{title}</h1>
@@ -23,15 +23,15 @@ const DetailedBlog = () => {
                         </div>
                     </div>
                     {/* tabs */}
-                    <div>
-                        <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap dark:bg-gray-100 dark:text-gray-800">
-                            <Link onClick={() => setActiveCard(0)}  className={`flex items-center  flex-shrink-0 px-5 py-3 space-x-2  dark:text-gray-600 ${activeCard === 0 ? 'border border-b-0' : 'border-b'}`}>
+                    <div className="">
+                        <div className="flex items-center  overflow-x-auto overflow-y-hidden   sm:justify-start flex-nowrap dark:bg-gray-100 dark:text-gray-800">
+                            <Link onClick={() => setActiveCard(0)}  className={`flex items-center   flex-shrink-0 px-5 py-3 space-x-2  dark:text-gray-600 ${activeCard === 0 ? 'border border-b-0' : 'border-b'}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                                     <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                                 </svg>
                                 <span>Content</span>
                             </Link>
-                            <Link to='author' onClick={() => setActiveCard(1)} rel="noopener noreferrer" href="#" className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2   rounded-t-lg  dark:text-gray-900 ${activeCard === 1 ? 'border border-b-0' : 'border-b'}`}>
+                            <Link to='author' onClick={() => setActiveCard(1)}  className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2   rounded-t-lg   dark:text-gray-900 ${activeCard === 1 ? 'border border-b-0' : 'border-b'}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                                     <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                                     <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
@@ -45,27 +45,7 @@ const DetailedBlog = () => {
                        <Outlet></Outlet>
                     </div>
                 </article>
-                <div>
-                    <div className="flex flex-wrap py-6 gap-2 border-t border-dashed dark:border-gray-600">
-                        <a rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#MambaUI</a>
-                        <a rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#TailwindCSS</a>
-                        <a rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#Angular</a>
-                    </div>
-                    <div className="space-y-2">
-                        <h4 className="text-lg font-semibold">Related posts</h4>
-                        <ul className="ml-4 space-y-1 list-disc">
-                            <li>
-                                <a rel="noopener noreferrer" href="#" className="hover:underline">Nunc id magna mollis</a>
-                            </li>
-                            <li>
-                                <a rel="noopener noreferrer" href="#" className="hover:underline">Duis molestie, neque eget pretium lobortis</a>
-                            </li>
-                            <li>
-                                <a rel="noopener noreferrer" href="#" className="hover:underline">Mauris nec urna volutpat, aliquam lectus sit amet</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                
             </div>
         </div>
     );
