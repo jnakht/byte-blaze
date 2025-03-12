af1662ebed2631a566f4a33b38719c2bc4de7be7
jnakht <jisan.cse.cu@gmail.com>
2025-03-12 15:45:17 +0600

    bookmarded items shown

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+import { getFromLocalStorage } from "../../Utility/Utility";
+import BlogCard from "../BlogCard/BlogCard";
 
 
 const Bookmarks = () => {
+    const [bookmarks, setbookmarks] = useState([]);
+    useEffect(() => {
+        const blogs = getFromLocalStorage();
+        setbookmarks(blogs);
+    } ,[])
     return (
         <div>
             <h3>This is bookmarks section</h3>
+            <div className="grid grid-cols-3 max-w-[80vw] mx-auto gap-6 p-6">
+            {
+                bookmarks.map(bookmark => <BlogCard blog={bookmark}></BlogCard>)
+            }
+            </div>
         </div>
     );
 };
