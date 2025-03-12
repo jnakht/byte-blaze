3372591dc9c8ef9d1ae27ad88c39cce7df7bf795
jnakht <jisan.cse.cu@gmail.com>
2025-03-12 22:17:26 +0600

    link to original blog

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -6,7 +6,7 @@ import rehypeRaw from "rehype-raw";
 
 const Content = () => {
     const blog = useLoaderData();
-    const { cover_image, title, tags, body_html } = blog;
+    const { cover_image, title, tags, body_html, url } = blog;
 
     console.log("HTML Content:", body_html); // Debugging
 
@@ -25,7 +25,7 @@ const Content = () => {
             </div>
             
             <div className="p-6 space-y-2">
-                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+                <a href={url} target='_blank' className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</a>
             </div>
 
            <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{body_html}</Markdown>
