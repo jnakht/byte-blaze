c6f19b34107612510f178afc3f95327631d6ea9c
jnakht <jisan.cse.cu@gmail.com>
2025-03-12 20:36:58 +0600

    no bookmark found message shown

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getFromLocalStorage } from "../../Utility/Utility";
 import BlogCard from "../BlogCard/BlogCard";
+import NoBookmark from "../NoBookmark/NoBookmark";
 
 
 const Bookmarks = () => {
@@ -11,9 +12,12 @@ const Bookmarks = () => {
         setbookmarks(blogs);
     } ,[changeState])
     return (
-        <div>
-            <h3>This is bookmarks section</h3>
-            <div className="grid grid-cols-3 max-w-[80vw] mx-auto gap-8 p-6">
+        <div className="min-h-[calc(100vh-120px)]">
+            {
+                bookmarks.length === 0 && <NoBookmark></NoBookmark>
+            }
+            
+            <div className="grid grid-cols-3 max-w-[80vw] mx-auto gap-8 p-6 ">
             {
                 bookmarks.map(bookmark => <BlogCard changeState={changeState} setChangeState={setChangeState} isdelete={true} blog={bookmark}></BlogCard>)
             }
